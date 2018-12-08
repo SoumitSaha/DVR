0bdf8800a5cff3e41b4d161ad47f3f25ad0ef158
SoumitSaha <1505047.sks@ugrad.cse.buet.ac.bd>
2018-12-08 18:14:13 +0600

    socket and show command of driver done

--- 1505047.cpp
+++ 1505047.cpp
@@ -6,11 +6,16 @@
 #include <vector>
 #include <set>
 #include <fstream>
+#include <bits/stdc++.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #define INF -1
 
 using namespace std;
 string this_router_ip;
-
+int sockfd;
+int no_of_bytes_received;
 set <string> routers_in_network;
 
 struct table_entry{
@@ -115,11 +120,40 @@ void print_route_table(){
 	}
 }
 
+void print_neighbours(){
+	cout << this_router_ip << endl;
+	cout << "Neighbours :" << endl;
+	//cout << "-------------\t-------------\t-----" << endl;
+	for (int i = 0; i < neighbour_list.size(); ++i){
+		//if(!route_table[i].dest.compare(this_router_ip)) continue;
+		cout << "ip : " << neighbour_list[i].ip << " Cost : " << neighbour_list[i].cost << endl;
+	}
+}
+
 void print_routers(){
 	set<string>::iterator it;
 	cout << this_router_ip << "--->" << endl;
 	for (it = routers_in_network.begin(); it != routers_in_network.end(); ++it){
 		cout << *it << endl;
+	}
+}
+
+void start_router(){
+	struct sockaddr_in router;
+	socklen_t address_length;
+
+	while(true){
+		char buffer[1024];
+		no_of_bytes_received = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*) &router, &address_length);
+		if(no_of_bytes_received != -1){
+			string msg(buffer);
+			//cout << msg << endl;
+			string head = msg.substr(0,4);
+			if(!head.compare("show")){
+				print_route_table();
+			}
+			
+		}
 	}
 }
 
@@ -130,11 +164,27 @@ int main(int argc, char* argv[]){
 		exit(1);
 	}
 
-	
+
 	this_router_ip = argv[1];
 	string topology_file;
 	topology_file = argv[2];
 	list_neighbours(this_router_ip, topology_file);
 	make_initial_routing_table(this_router_ip);
 	print_route_table();
+	//print_routers();
+	//print_neighbours();
+
+	int bind_flag;
+	struct sockaddr_in client;
+	client.sin_family = AF_INET;
+	client.sin_port = htons(4747);
+	inet_pton(AF_INET, argv[1], &client.sin_addr);
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	bind_flag = bind(sockfd, (struct sockaddr*) &client, sizeof(sockaddr_in));
+	if(!bind_flag) cout << "Connection Successful." << endl;
+	else cout << "Connection failed." << endl;
+	
+	start_router();
 }
